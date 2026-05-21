d06fcbb3df783d63607142f5eb2d5a99cd85dde1
Greaves-G <g3019850@southstaffs.ac.uk>
2026-05-21 16:01:05 +0100

    made more menu's for a tutorial

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,13 +1,17 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
 public class Buttons : MonoBehaviour
 {
     public GameObject gameObjectButton;
+    public GameObject Arrow;
+    public GameObject oldPage;
+    public GameObject newPage;
     public bool currentState = false;
     public TMP_Text buttonText;
-    public string textWhenOn = "Switch On";
-    public string textWhenOff = "Switch Off";
+    //public string textWhenOn = "Switch On";
+    //public string textWhenOff = "Switch Off";
 
     private void Start()
     {
@@ -23,6 +27,25 @@ public class Buttons : MonoBehaviour
         gameObjectButton.SetActive(!gameObjectButton.activeInHierarchy);
     }
 
+    public void NextPageArrow()
+    {
+        StartCoroutine(NextPageArrowRoutine());
+    }
+
+
+    public IEnumerator NextPageArrowRoutine()
+    {
+        currentState = !currentState;
+
+        newPage.SetActive(!newPage.activeInHierarchy);
+        yield return new WaitForSeconds(0.1f);
+        oldPage.SetActive(!oldPage.activeInHierarchy);
+        yield return new WaitForSeconds(0.1f);
+        Arrow.SetActive(!Arrow.activeInHierarchy);  
+    }
+    
+    
+    
     /*void updateElement()
     {
         gameObjectButton.SetActive(currentState);
